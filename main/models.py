83388bb7f953880676ac3a72076f35edf87062fc
ayman <kabbachayman95@gmail.com>
2022-11-10 23:54:01 +0100

    create Category model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    Picture=models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
